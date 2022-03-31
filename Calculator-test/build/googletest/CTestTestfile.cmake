88e719baaa597b9e429b341b33ff10c68ab95928
Amit Yadav <amit.yadav05@nagarro.com>
2022-03-31 18:49:50 +0530

    Updating test
    removing extra file present in calculator-test

--- Calculator-test/build/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/d/Go_Basic/projects/go/src/C++ Project/sqliteWithC++/Calculator-test/googletest
-# Build directory: /mnt/d/Go_Basic/projects/go/src/C++ Project/sqliteWithC++/Calculator-test/build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
