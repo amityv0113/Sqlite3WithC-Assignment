d190e6d3c8d8b8fb20498ec047534f37c4fc7476
Amit Yadav <amit.yadav05@nagarro.com>
2022-04-07 19:42:01 +0530

    Update calculatorClass
    adding error handling for integer overflow condition
    adding enum

--- CalculatorClass/Calculator.h
+++ CalculatorClass/Calculator.h
@@ -2,17 +2,23 @@
 #define _BITSTDC_
 #include<bits/stdc++.h>
 #endif
-
-#include "../CalculatorDbModel/CalculatorModel.h"
-
 #ifndef CALCULATOR_H
 #define CALCULATOR_H
+#include "../CalculatorDbModel/CalculatorModel.h"
+#include <memory>
 
 class DividByZeroException : public std::exception{  
     public:  
         const char * what() const throw()  
         {  
             return "Attempted to divide by zero!\n";  
+        }  
+}; 
+class OverFlow : public std::exception{  
+    public:  
+        const char * what() const throw()  
+        {  
+            return "overflow condition \n";  
         }  
 }; 
 
@@ -22,8 +28,31 @@ class Sum{
         Sum(){};
         T sum(T num1,T num2)
         {
-            T _sum =num1+num2;
+            T _sum;
+            try{
+                 _sum =num1+num2;
+                 if (num1>0 && num2>0 && _sum<0)
+                 {
+                    OverFlow overflow;
+                    throw overflow;
+                 }
+                 else if (num1<0 && num2<0 && _sum>0)
+                 {
+                    OverFlow overflow;
+                    throw overflow;
+                 }
+                 else
+                 {
+                     return _sum;
+                 }
+                // return _sum;
+            }
+            catch(std::exception& e)
+            {
+                std::cout<<e.what()<<std::endl;
+            }
             return _sum;
+           
         }
         // function overloading 
         T sum(std::initializer_list<T> list)
@@ -44,7 +73,28 @@ class Mul{
         Mul(){};
         T mul(T num1,T num2)
         {
-            T _mul =num1*num2;
+            T _mul;
+            try{
+                _mul =num1*num2;
+                if (num1>0 && num2>0 && _mul<0)
+                {
+                    OverFlow overflow;
+                    throw overflow;
+                }
+                else if (num1<0 && num2<0 && _mul>0)
+                {
+                    OverFlow overflow;
+                    throw overflow;
+                }
+                else
+                {
+                    return _mul;
+                }
+            }
+            catch(std::exception& e)
+            {
+                std::cout<<e.what()<<std::endl;
+            }
             return _mul;
         }
         
@@ -93,20 +143,27 @@ class Calculator:public Sum<T>,public Mul<T>,public Sub<T>,public Div<T>  // cal
 {
     T num1;
     T num2;
-    const char* DirectoryLocation;
+    std::string DirectoryLocation;
     // location where we want to create calculator.db file 
-    DbConnection*ptrDb =nullptr;
-
+    // DbConnection*ptrDb =nullptr;
+    std::shared_ptr<DbConnection>ptrDb;
     //checks if you want to update database or not check=false ->do not want to update check=true want to update database
     bool check=false;
     public:
         Calculator(T _num1 ,T _num2):num1{_num1},num2{_num2}{
             this->check=false;
         }
-        Calculator(T _num1 ,T _num2,const char*s):num1{_num1},num2{_num2},DirectoryLocation{s}{   // constructor for calculator
+        Calculator(T _num1 ,T _num2,std::string&s):num1{_num1},num2{_num2},DirectoryLocation{s}{   // constructor for calculator
             this->check=true;
-            ptrDb=DbConnection::start(DirectoryLocation);
+            std::unique_ptr<DbConnection>ptrDbTemp(DbConnection::start(DirectoryLocation.c_str()));
+            ptrDb=std::move(ptrDbTemp);
         }
+        enum ArithmeticOperation{
+            temp_add=1, 
+            temp_sub=2, 
+            temp_mul=3, 
+            temp_div=4
+        };
         T calculate_operation(int val)
         {
             T result;
@@ -116,44 +173,66 @@ class Calculator:public Sum<T>,public Mul<T>,public Sub<T>,public Div<T>  // cal
             std::string _result;
             switch (val)
             {
-                case 1:
+                case ArithmeticOperation::temp_add:
                     result= this->sum(num1,num2);
                     _op="+";
                     _result=std::to_string(result);
-                    if (check==true)
+                    if (check)
                     {
-                        ptrDb->InsertData(_num1,_num2,_op,_result);
+                        if ((num1==-num1)&& result==0)
+                        {
+                            ptrDb->InsertData(_num1,_num2,_op,_result);
+                        }
+                        else if(result!=0)
+                        {
+                            ptrDb->InsertData(_num1,_num2,_op,_result);
+                        }  
                     }
                     
                     break;                
-                case 2:
+                case ArithmeticOperation::temp_sub:
                     result= this->sub(num1,num2);
                     _op="-";
                     _result=std::to_string(result);
-                    if(check)
+                    if(check )
                     {
                         ptrDb->InsertData(_num1,_num2,_op,_result);
                     }
                     break;
-                case 3:
+                case ArithmeticOperation::temp_mul:
                     result= this->mul(num1,num2);
                     _op="*";
                     _result=std::to_string(result);
-                    if(check==true)
+                    if(check)
                     {
-                        ptrDb->InsertData(_num1,_num2,_op,_result);
+                        if (result==0 && (num1==0 || num2==0))
+                        {
+                            ptrDb->InsertData(_num1,_num2,_op,_result);
+                        }
+                        else if (result!=0)
+                        {
+                            ptrDb->InsertData(_num1,_num2,_op,_result);
+                        }
+                        
                     }
                     break;
-                case 4:
+                case ArithmeticOperation::temp_div:
                     result= this->div(num1,num2);
                     _op="/";
                     _result=std::to_string(result);
-                    if(check==true)
+                    if(check)
                     {
-                        ptrDb->InsertData(_num1,_num2,_op,_result);
+                        if (result==0 && num1==0)
+                        {
+                            ptrDb->InsertData(_num1,_num2,_op,_result);
+                        }
+                        else if (result!=0)
+                        {
+                            ptrDb->InsertData(_num1,_num2,_op,_result);
+                        }
                     }
                     break;
-
+                
             }
             return result;
         }
@@ -163,7 +242,7 @@ class Calculator:public Sum<T>,public Mul<T>,public Sub<T>,public Div<T>  // cal
             bool flag=true;
             while(flag)
             {
-                operationToPerform(val);
+                val=operationToPerform();
                 std::cout<<"Result : "<<calculate_operation(val)<<std::endl;
                 std::cout<<"if you want to exit program enter -1"<<std::endl;
                 int temp;
@@ -175,13 +254,33 @@ class Calculator:public Sum<T>,public Mul<T>,public Sub<T>,public Div<T>  // cal
             }
         }
 
-        void operationToPerform(int&val)
+        int operationToPerform()
         {
+            int val;
             std::cout<<"enter 1 for addition"<<std::endl;
             std::cout<<"enter 2 for subtraction"<<std::endl;
             std::cout<<"enter 3 for multiplication"<<std::endl;
             std::cout<<"enter 4 for divsion"<<std::endl;
             std::cin>>val;
+            bool temp_flag=true;
+            if ((val==1 || val==2 || val==3 || val==4))
+            {
+                return val;
+            }
+            while(temp_flag)
+            {
+                std::cin.clear();// clears the error flag on cin (so that future I/O operations will work correctly),
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                std::cout<<"You have entered wrong input! please enter valid input :" <<std::endl;
+                std::cin>>val;
+                if ((val==1 || val==2 || val==3 || val==4))
+                {
+                    temp_flag=false;
+                    break;
+                }
+                std::cout<<val<<std::endl;
+            }
+            return val;
         }
 
         void showAllOperationHistory(){
